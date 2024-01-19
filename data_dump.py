c91bb5454dca6579819513eb7afafd9233a6aaae
Rushi9867 <rushikhandare2203@gmail.com>
2024-01-19 13:35:58 +0530

    updated

--- data_dump.py
+++ data_dump.py
@@ -2,8 +2,8 @@ import pymongo
 import pandas as pd
 import json
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017")
-#client = pymongo.MongoClient("mongodb+srv://Rushikesh:Rushi9867@cluster0.f3rukrp.mongodb.net/?retryWrites=true&w=majority")
+#client = pymongo.MongoClient("mongodb://localhost:27017")
+client = pymongo.MongoClient("mongodb+srv://Rushi:Rushi9867@cluster0.eqcyv30.mongodb.net/?retryWrites=true&w=majority")
 
 DATA_FILE_PATH="/config/workspace/Data/train.csv"
 DATABASE_NAME="diamond"
